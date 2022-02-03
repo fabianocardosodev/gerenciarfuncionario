bbbb8cbf8ffeb324389ff566d1e3593bc48432d1
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2022-02-03 11:29:38 -0200

    definindo requisições

--- gerenciar/gerfuncion/views.py
+++ gerenciar/gerfuncion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from gerenciar.gerenciar.models import Funcionario
+
+class ListaFuncionarios(ListView):
+    template_name = "templates/funcionarios.html"
+    model = Funcionario
+    context_object_name ="funcionarios"
+
+
+
 
 # Create your views here.
